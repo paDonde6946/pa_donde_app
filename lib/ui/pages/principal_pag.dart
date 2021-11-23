69f358d3cbec0878d338ca1b86ab5aca0732c22d
Hernan Enrique Gordillo Garzòn <Hernan Gordillo>
2021-11-22 23:55:48 -0500

    Princial

--- lib/ui/pages/principal_pag.dart
+++ lib/ui/pages/principal_pag.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:intl/intl.dart';
 
 //------------------IMPORTACIONES LOCALES------------------------------
 import 'package:pa_donde_app/ui/global_widgets/show_dialogs/confirmacion_show.dart';
@@ -19,7 +20,7 @@ class _PrincipalPagState extends State<PrincipalPag> {
   @override
   Widget build(BuildContext context) {
     setState(() {});
-    return Scaffold(body: body());
+    return Scaffold(appBar: appBar(), body: body());
   }
 
   Widget body() {
@@ -38,8 +39,126 @@ class _PrincipalPagState extends State<PrincipalPag> {
     //   });
     // }
 
-    return const Center(
-      child: Text("Principal"),
+    return ListView(
+      children: [
+        cardDeServicio(
+            titulo: "Pa Donde",
+            destino: "Calle 74 A - No. 113 A - 47",
+            origen: "Calle 74 A - No. 113 A - 47",
+            fecha: DateTime.parse("1969-07-20 20:18:04Z")),
+        cardDeServicio(
+            titulo: "Pa Donde",
+            destino: "Calle 74 A - No. 113 A - 47",
+            origen: "Calle 74 A - No. 113 A - 47",
+            fecha: DateTime.parse("1969-07-20 20:18:04Z"))
+      ],
+    );
+  }
+
+  Widget cardDeServicio(
+      {String? titulo, String? origen, String? destino, DateTime? fecha}) {
+    return Card(
+      // color: Theme.of(context).primaryColor.withOpacity(0.35),
+      color: const Color.fromRGBO(238, 246, 232, 1),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      margin: const EdgeInsets.all(15),
+      elevation: 3,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(right: 30, left: 30, top: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                tituloDelServicio(titulo: titulo),
+                Row(children: [
+                  const Icon(Icons.access_time_outlined, size: 20),
+                  textoDelServicio(
+                      texto: DateFormat('hh:mm a').format(DateTime.now()))
+                ]),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 40, top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      subTitulosDelServicio(subtitulo: "Origen"),
+                      textoDelServicio(texto: origen),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      subTitulosDelServicio(subtitulo: "Destino"),
+                      textoDelServicio(texto: destino)
+                    ],
+                  ),
+                ),
+                botonDelServicio(nombreBoton: "Ver mas")
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  PreferredSizeWidget appBar() {
+    final size = MediaQuery.of(context).size;
+    return AppBar(
+        centerTitle: true,
+        foregroundColor: Colors.black,
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text(
+          "PaDonde",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ));
+  }
+
+  Text tituloDelServicio({titulo}) {
+    return Text(titulo,
+        style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Theme.of(context).primaryColor));
+  }
+
+  Widget subTitulosDelServicio({subtitulo}) {
+    return Text(subtitulo,
+        textAlign: TextAlign.start,
+        style: TextStyle(
+            fontWeight: FontWeight.w500,
+            color: Theme.of(context).primaryColor));
+  }
+
+  Widget textoDelServicio({texto}) {
+    return Text(texto);
+  }
+
+  Widget botonDelServicio({String nombreBoton = ''}) {
+    return Container(
+      height: 40,
+      width: 100,
+      // padding: EdgeInsets.only(bottom: ),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.only(
+            bottomRight: Radius.circular(10), topLeft: Radius.circular(10)),
+        color: Theme.of(context).primaryColorLight,
+      ),
+      child: IconButton(
+        icon: Text(
+          nombreBoton,
+          style: const TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        onPressed: () => {},
+      ),
     );
   }
 }
